4bca99bbd408be5774dc94b28b42f427d5609ac9
cirejosue@hotmail.com <>
2015-12-16 22:50:53 -0600

    MenuInicial

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="CSS/MenuPrincipalCSS.css" type="text/css" media="screen">
+        <title></title>
+    </head>
+    <body>
+       
+        
+        <div id="menuLateral" style="background: url(Imagenes/leftMenu.jpeg); background-size: cover;">
+            
+              <ul id="ListaMenuLateral">
+                <li><a>CLUBES PRO</a></li>
+                <li><a>TORNEOS VIGENTES</a></li>
+                <li><a>CLUBES</a></li>
+                <li><a>JUGADORES</a></li>
+                <li><a>TRANSFERENCIAS</a></li>
+                <li><a>RANKING POR CLUBES</a></li>
+                 
+            </ul>
+            
+            
+        </div>
+     
+     
+        
+            
+        <div id="menuSuperior" style="background:url(Imagenes/topMenu.jpeg); background-size: cover;"> 
+        
+            <ul id="ListaMenuSuperior">
+                <li><a>CLUBES PRO</a></li>
+                <li><a>1 VS 1</a></li>
+                <li><a>REGLAMENTO</a></li>
+                <li><a>SALA DE TROFEOS</a></li>
+                <li><a>NOTICIAS</a></li>
+                <li><a>LOGIN</a></li>
+                 
+            </ul>
+        
+        
+        </div>
+        
+        <div id="menuCentral" style="background:url(Imagenes/middleMenu.jpeg); background-size: cover;" >
+                 
+            
+            
+        </div>
+        
+    </body>
+</html>
